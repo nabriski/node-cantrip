7f86c5d4616a5d0f0fc7bce0099cec2a307f0b82
nabriski <itamar@nabriski.me>
2019-01-13 23:01:39 +0200

    move bash out

--- index.js
+++ index.js
@@ -1,41 +1,22 @@
 #!/usr/bin/env node
 const getStdin  = require("get-stdin");
 const {NodeVM}  = require('vm2');
-const { execSync } = require('child_process');
-
-const sep_uuid = "eb0d83980a0511e98a8dab819760380e";
-const headers_sep = `headers_${sep_uuid}`
-
-let bash = function(str){
-    return execSync(str,{shell:'/bin/rbash'});
-}
 
 // Optionally, you can export the implementation of the command like so:
-getStdin().then(script => {
+getStdin().then(stdin => {
 
     const write = function(out){
         process.stdout.write(out,{encoding:'utf-8'});
     }
 
-    const writeHeaders = function(headers){
-
-        process.stdout.write(`\n${headers_sep}\n`)
-        Object
-            .keys(headers)
-            .forEach(function(h){
-                process.stdout.write([h,headers[h]].join(": ")+"\n")
-            });
-        process.stdout.write(`${headers_sep}\n`)
-    }
     const vm = new NodeVM({
         console: 'inherit',
         sandbox: {
             env:process.env,
-            bash:bash,
-            write:write,
-            headers:writeHeaders
+            stdin : stdin,
+            write:write
         }
     });
-    vm.run(script);
+    vm.run(process.argv[2]);
 
 });
--- test/test-cli.js
+++ test/test-cli.js
@@ -1,39 +1,11 @@
 const shell = require('shelljs');
 const tap = require('tap');
 
-let ret = shell.exec("echo \"write('1')\" | ./index.js",{silent:true}).stdout;
+let ret = shell.exec("./index.js \"write('1')\" < /dev/null",{silent:true}).stdout;
 tap.equal(ret,"1","value is 1");
 
-ret = shell.exec("echo \"write(bash('echo -n hello').toString())\" | ./index.js",{silent:true}).stdout;
-tap.equal(ret,"hello","echo value is hello");
 
+ret = shell.exec("echo '{\"name\": \"Aaron Saunders\"}' | ./index.js \"let name = JSON.parse(stdin).name;write(name)\"",{silent:true}).stdout;
+tap.equals(ret,"Aaron Saunders","JSON with a name attribute");
 
-ret = shell.exec("echo \"write(bash('curl -s https://api.datashim.com/pseudo-heroism').toString())\" | ./index.js",{silent:true}).stdout;
-tap.ok(JSON.parse(ret).name,"JSON with a name attribute");
-
-//image
-ret = shell.exec("echo \"let img=bash('convert -size 100x100 xc:white png:-');write(img)\" | ./index.js",{silent:true,encoding:'buffer'}).stdout;
-tap.equal(ret.length,273,"image size is 273 bytes");
-
-//headers
-ret = shell.exec("echo \"headers({some_header:123});write('1\\\n')\" | ./index.js",{silent:true}).stdout;
-let expected = [
-    "",
-    "headers_eb0d83980a0511e98a8dab819760380e",
-    "some_header: 123",
-    "headers_eb0d83980a0511e98a8dab819760380e",
-    "1"
-].join("\n");
-
-tap.equal(ret,expected,"value is 1 with custom header");
-
-ret = shell.exec("echo \"write('1');write('3');headers({some_header:123});write('2')\" | ./index.js",{silent:true}).stdout;
-expected = [
-    "13",
-    "headers_eb0d83980a0511e98a8dab819760380e",
-    "some_header: 123",
-    "headers_eb0d83980a0511e98a8dab819760380e",
-    "2"
-].join("\n");
-tap.equals(ret,expected,"can write headers wherever");
 
